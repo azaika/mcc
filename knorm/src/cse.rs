a6d5f6e77983e4b14258cf7b48754e4f0f995254
azaika <takaaza@gmail.com>
2021-12-24 21:17:56 +0900

    make CSE faster

--- knorm/src/cse.rs
+++ knorm/src/cse.rs
@@ -18,14 +18,14 @@ fn eq_in_val(e: &Expr, other: &Expr) -> bool {
                 Add | FAdd | Mul | FMul => (x1, x2) == (y1, y2) || (x2, x1) == (y1, y2),
                 Sub | FSub | Div | FDiv => (x1, x2) == (y1, y2),
             }
-        },
+        }
         (If(k1, x1, x2, e1, e2), If(k2, y1, y2, e3, e4)) if k1 == k2 => {
             let comp_eq = match k1 {
                 IfKind::IfEq => (x1, x2) == (y1, y2) || (x1, x2) == (y2, y1),
                 IfKind::IfLE => (x1, x2) == (y1, y2),
             };
             comp_eq && eq_in_val(e1, e3) && eq_in_val(e2, e4)
-        },
+        }
         (Let(l1), Let(l2)) => {
             match (l1, l2) {
                 (LetKind::Let(d1, e1, e2), LetKind::Let(d2, e3, e4)) => {
@@ -38,18 +38,28 @@ fn eq_in_val(e: &Expr, other: &Expr) -> bool {
                             rename(e4.clone(), &m);
                             eq_in_val(e2, &e4)
                         }
-                    }
-                    else {
+                    } else {
                         false
                     }
-                },
-                _ => l1 == l2 // for simplicity
+                }
+                _ => l1 == l2, // for simplicity
             }
-        },
-        (Loop { vars: xs, init: i1, body: e1 }, Loop { vars: ys, init: i2, body: e2 }) => {
+        }
+        (
+            Loop {
+                vars: xs,
+                init: i1,
+                body: e1,
+            },
+            Loop {
+                vars: ys,
+                init: i2,
+                body: e2,
+            },
+        ) => {
             xs == ys && i1 == i2 && e1 == e2 // for simplicity
-        },
-        _ => e.item == other.item
+        }
+        _ => e.item == other.item,
     }
 }
 
@@ -66,7 +76,10 @@ fn hash_impl<H: std::hash::Hasher>(e: &Expr, state: &mut H, num_let: usize) {
     match &e.item {
         Const(c) => c.hash(state),
         Var(x) | ExtArray(x) => x.hash(state),
-        UnOp(op, x) => {op.hash(state); x.hash(state)},
+        UnOp(op, x) => {
+            op.hash(state);
+            x.hash(state)
+        }
         BinOp(op, x, y) => {
             op.hash(state);
             use BinOpKind::*;
@@ -77,7 +90,7 @@ fn hash_impl<H: std::hash::Hasher>(e: &Expr, state: &mut H, num_let: usize) {
 
             x.hash(state);
             y.hash(state)
-        },
+        }
         If(kind, x, y, e1, e2) => {
             kind.hash(state);
             let (x, y) = if x < y { (x, y) } else { (y, x) };
@@ -85,53 +98,49 @@ fn hash_impl<H: std::hash::Hasher>(e: &Expr, state: &mut H, num_let: usize) {
             y.hash(state);
             hash_impl(&e1, state, num_let);
             hash_impl(&e2, state, num_let)
-        },
-        Let(l) => {
-            match l {
-                LetKind::Let(d, e1, e2) => {
+        }
+        Let(l) => match l {
+            LetKind::Let(d, e1, e2) => {
+                const HASHLING_LIMIT: usize = 5;
+                if num_let < HASHLING_LIMIT {
                     hash_impl(&e1, state, num_let);
-
                     let new_name = format!("V{}{}", ty::knormal::short(&d.t), num_let);
-
                     new_name.hash(state);
-
                     let mut m = util::Map::default();
                     m.insert(d.name.clone(), new_name);
-
                     let e2 = rename(Box::new(e2.as_ref().clone()), &m);
-
                     hash_impl(&e2, state, num_let + 1)
-                },
-                LetKind::LetRec(Fundef { fvar, args, body }, e2) => {
-                    fvar.hash(state);
-                    args.hash(state);
-                    hash_impl(&body, state, num_let);
-                    hash_impl(&e2, state, num_let);
-                },
-                LetKind::LetTuple(ds, x, e2) => {
-                    ds.hash(state);
-                    x.hash(state);
-                    hash_impl(&e2, state, num_let);
-                },
+                }
+            }
+            LetKind::LetRec(Fundef { fvar, args, body }, e2) => {
+                fvar.hash(state);
+                args.hash(state);
+                hash_impl(&body, state, num_let);
+                hash_impl(&e2, state, num_let);
+            }
+            LetKind::LetTuple(ds, x, e2) => {
+                ds.hash(state);
+                x.hash(state);
+                hash_impl(&e2, state, num_let);
             }
         },
         Tuple(xs) => xs.hash(state),
         App(f, args) | ExtApp(f, args) => {
             f.hash(state);
             args.hash(state)
-        },
+        }
         CreateArray(x, y) | Get(x, y) => {
             x.hash(state);
             y.hash(state)
-        },
+        }
         Put(x, y, z) => {
             x.hash(state);
             y.hash(state);
             z.hash(state)
-        },
+        }
         Loop { .. } | Continue(_) => {
             // do nothing because loop is not target of CSE
-        },
+        }
     }
 }
 
@@ -148,33 +157,39 @@ fn is_impure(e: &Expr, effects: &mut Set) -> bool {
     use ExprKind::*;
     match &e.item {
         If(_, _, _, e1, e2) => is_impure(e1, effects) || is_impure(e2, effects),
-        Let(l) => {
-            match l {
-                LetKind::Let(_, e1, e2) => is_impure(e1, effects) || is_impure(e2, effects),
-                LetKind::LetRec(fundef, e2) => {
-                    if is_impure(&fundef.body, effects) {
-                        effects.insert(fundef.fvar.name.clone());
-                    }
+        Let(l) => match l {
+            LetKind::Let(_, e1, e2) => is_impure(e1, effects) || is_impure(e2, effects),
+            LetKind::LetRec(fundef, e2) => {
+                if is_impure(&fundef.body, effects) {
+                    effects.insert(fundef.fvar.name.clone());
+                }
 
-                    let r = is_impure(e2, effects);
+                let r = is_impure(e2, effects);
 
-                    effects.remove(&fundef.fvar.name);
+                effects.remove(&fundef.fvar.name);
 
-                    r
-                },
-                LetKind::LetTuple(_, _, e2) => is_impure(e2, effects),
+                r
             }
+            LetKind::LetTuple(_, _, e2) => is_impure(e2, effects),
         },
         App(f, _) => effects.contains(f),
-        ExtApp(_, _) | CreateArray(_, _) | ExtArray(_) | Put(_, _, _) | Get(_, _) | Loop { .. } => true,
-        _ => false
+        ExtApp(_, _) | CreateArray(_, _) | ExtArray(_) | Put(_, _, _) | Get(_, _) | Loop { .. } => {
+            true
+        }
+        _ => false,
     }
 }
 
 fn conv(mut e: Box<Expr>, effects: &mut Set, saved: &mut Map) -> Box<Expr> {
     use ExprKind::*;
     e.item = match e.item {
-        If(kind, x, y, e1, e2) => If(kind, x, y, conv(e1, effects, saved), conv(e2, effects, saved)),
+        If(kind, x, y, e1, e2) => If(
+            kind,
+            x,
+            y,
+            conv(e1, effects, saved),
+            conv(e2, effects, saved),
+        ),
         Let(l) => {
             let kind = match l {
                 LetKind::Let(d, e1, e2) => {
@@ -183,22 +198,25 @@ fn conv(mut e: Box<Expr>, effects: &mut Set, saved: &mut Map) -> Box<Expr> {
                         log::info!("found common sub-expressions `{}` and `{}`.", d.name, x);
                         let mut m = util::Map::default();
                         m.insert(d.name.clone(), x.clone());
-                        LetKind::Let(d, Box::new(Spanned::new(Var(x.clone()), e.loc)), conv(e2, effects, saved))
-                    }
-                    else {
+                        LetKind::Let(
+                            d,
+                            Box::new(Spanned::new(Var(x.clone()), e.loc)),
+                            conv(e2, effects, saved),
+                        )
+                    } else {
                         let e1 = key.0;
-                        let key = ExprInVal(e1.clone());
                         if !is_impure(&e1, effects) {
+                            let key = ExprInVal(e1.clone());
                             saved.insert(key.clone(), d.name.clone());
+                            let r =
+                                LetKind::Let(d, conv(e1, effects, saved), conv(e2, effects, saved));
+                            saved.remove(&key);
+                            r
+                        } else {
+                            LetKind::Let(d, conv(e1, effects, saved), conv(e2, effects, saved))
                         }
-
-                        let r = LetKind::Let(d, conv(e1, effects, saved), conv(e2, effects, saved));
-
-                        saved.remove(&key);
-
-                        r
                     }
-                },
+                }
                 LetKind::LetRec(Fundef { fvar, args, body }, e2) => {
                     // この宣言が副作用を持つか調べる
                     if is_impure(&body, effects) {
@@ -208,14 +226,18 @@ fn conv(mut e: Box<Expr>, effects: &mut Set, saved: &mut Map) -> Box<Expr> {
                     let body = conv(body, effects, saved);
                     let e2 = conv(e2, effects, saved);
                     LetKind::LetRec(Fundef { fvar, args, body }, e2)
-                },
+                }
                 LetKind::LetTuple(ds, x, e2) => LetKind::LetTuple(ds, x, conv(e2, effects, saved)),
             };
 
             Let(kind)
+        }
+        Loop { vars, init, body } => Loop {
+            vars,
+            init,
+            body: conv(body, effects, saved),
         },
-        Loop { vars, init, body } => Loop { vars, init, body: conv(body, effects, saved) },
-        _ => e.item
+        _ => e.item,
     };
 
     e
