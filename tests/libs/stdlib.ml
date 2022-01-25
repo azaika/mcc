323817e04e1786d7f37baeeb5c83f11fcdf18491
azaika <takaaza@gmail.com>
2022-01-25 18:42:26 +0900

    implement `print_int` in stdlib.ml

--- tests/libs/stdlib.ml
+++ tests/libs/stdlib.ml
@@ -51,15 +51,113 @@ in
 let rec print_newline x =
   x;
   let x = 10 in (* '\n' = 10 *)
-  _asmE"out" x; ()
-in
-let rec print_int x =
-  let a = x * 1 in
-  min_caml_print_int x; ()
+  _asmE"out" x;
+  ()
 in
 let rec print_char x =
   let a = x * 1 in
-  _asmE"out" x; ()
+  _asmE"out" x;
+  ()
+in
+let rec rem x a d =
+  if x >= a then rem (x - a) a (d + 1) else (d, x)
+in
+let rec print_byte x = (* assume `x < 256` *)
+  let x = if x >= 200 then (
+    print_char 50;
+    x - 200
+  ) else if x >= 100 then (
+    print_char 49;
+    x - 100
+  ) else
+    x
+  in
+  (* x < 100 *)
+  let (d, r) = rem x 10 0 in
+  if d = 0 then
+    print_char (r + 48)
+  else (
+    print_char (d + 48);
+    print_char (r + 48)
+  )
+in
+let rec print_int_final a index emerged =
+  if index >= 0 then (
+    let d = a.(index) in
+    if d = 0 then (
+      if emerged then print_char 48 else ();
+      print_int_final a (index - 1) emerged
+    )
+    else (
+      print_char (d + 48);
+      print_int_final a (index - 1) true
+    )
+  )
+  else ()
+in
+let rec print_int x =
+  let a = x * 1 in
+  let x = if x < 0
+    then (
+      print_char 45; (* '-' = 45 *)
+      -x
+    )
+    else
+      x
+  in
+  (* x >= 0 *)
+  if x = 0 then print_char 48
+  else if x < 256 then print_byte x
+  else (
+    let res = Array.make 10 0 in
+    let x = if x >= 1000000000 then (
+      let (d, r) = rem x 1000000000 0 in
+      res.(9) <- d;
+      r
+    ) else x in
+    let x = if x >= 100000000 then (
+      let (d, r) = rem x 100000000 0 in
+      res.(8) <- d;
+      r
+    ) else x in
+    let x = if x >= 10000000 then (
+      let (d, r) = rem x 10000000 0 in
+      res.(7) <- d;
+      r
+    ) else x in
+    let x = if x >= 1000000 then (
+      let (d, r) = rem x 1000000 0 in
+      res.(6) <- d;
+      r
+    ) else x in
+    let x = if x >= 100000 then (
+      let (d, r) = rem x 100000 0 in
+      res.(5) <- d;
+      r
+    ) else x in
+    let x = if x >= 10000 then (
+      let (d, r) = rem x 10000 0 in
+      res.(4) <- d;
+      r
+    ) else x in
+    let x = if x >= 1000 then (
+      let (d, r) = rem x 1000 0 in
+      res.(3) <- d;
+      r
+    ) else x in
+    let x = if x >= 100 then (
+      let (d, r) = rem x 100 0 in
+      res.(2) <- d;
+      r
+    ) else x in
+    let x = if x >= 10 then (
+      let (d, r) = rem x 10 0 in
+      res.(1) <- d;
+      r
+    ) else x in
+    res.(0) <- x;
+    print_int_final res 9 false
+  )
 in
 let rec read_float x =
   x;
